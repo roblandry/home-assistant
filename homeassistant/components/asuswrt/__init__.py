ea45d34f6775f766d04276e0e81dc036c69776f6
Rob Landry <rob.a.landry@gmail.com>
2019-11-09 11:29:52 -0500

    Add new asuswrt sensors

--- homeassistant/components/asuswrt/__init__.py
+++ homeassistant/components/asuswrt/__init__.py
@@ -26,7 +26,26 @@ DATA_ASUSWRT = DOMAIN
 DEFAULT_SSH_PORT = 22
 
 SECRET_GROUP = "Password or SSH Key"
-SENSOR_TYPES = ["upload_speed", "download_speed", "download", "upload"]
+SENSOR_TYPES = [
+    "upload_speed",
+    "download_speed",
+    "download",
+    "upload",
+    "dhcp",
+    "model",
+    "qos",
+    "reboot",
+    "wlan",
+    "2g_wifi",
+    "2g_guest_1",
+    "2g_guest_2",
+    "2g_guest_3",
+    "5g_wifi",
+    "5g_guest_1",
+    "5g_guest_2",
+    "5g_guest_3",
+    "firmware",
+]
 
 CONFIG_SCHEMA = vol.Schema(
     {
--- homeassistant/components/asuswrt/sensor.py
+++ homeassistant/components/asuswrt/sensor.py
@@ -7,6 +7,23 @@ from . import DATA_ASUSWRT
 
 _LOGGER = logging.getLogger(__name__)
 
+SENSOR_W_ATTRS = {
+    "dhcp",
+    "model",
+    "qos",
+    "reboot",
+    "wlan",
+    "2g_wifi",
+    "2g_guest_1",
+    "2g_guest_2",
+    "2g_guest_3",
+    "5g_wifi",
+    "5g_guest_1",
+    "5g_guest_2",
+    "5g_guest_3",
+    "firmware",
+}
+
 
 async def async_setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the asuswrt sensors."""
@@ -25,6 +42,9 @@ async def async_setup_platform(hass, config, add_entities, discovery_info=None):
         devices.append(AsuswrtRXSensor(api))
     if "upload_speed" in discovery_info:
         devices.append(AsuswrtTXSensor(api))
+    for attr_sensor in SENSOR_W_ATTRS:
+        if attr_sensor in discovery_info:
+            devices.append(AsuswrtSensorWAttrs(api, attr_sensor))
 
     add_entities(devices)
 
@@ -127,3 +147,83 @@ class AsuswrtTotalTXSensor(AsuswrtSensor):
         await super().async_update()
         if self._rates:
             self._state = round(self._rates[1] / 1000000000, 1)
+
+
+class AsuswrtSensorWAttrs(Entity):
+    """Representation of a asuswrt sensor with attributes."""
+
+    def __init__(self, api, attr_sensor):
+        """Initialize the sensor."""
+        self._api = api
+        self._state = None
+        self._name = f"Asuswrt {format_name(attr_sensor)}"
+        self._type = attr_sensor
+        self._sensor = attr_sensor.upper()
+        self._attributes = None
+
+    @property
+    def name(self):
+        """Return the name of the sensor."""
+        return self._name
+
+    @property
+    def state(self):
+        """Return the state of the sensor."""
+        return self._state
+
+    @property
+    def device_state_attributes(self):
+        """Return the attributes of the sensor."""
+        return self._attributes
+
+    async def async_update(self):
+        """Fetch new state data for the sensor."""
+        response = await self._api.async_get_nvram(self._sensor)
+
+        if self._type == "2g_wifi":
+            self._state = bool_onoff(response.pop("wl0_bss_enabled", None))
+        elif self._type == "2g_guest_1":
+            self._state = bool_onoff(response.pop("wl0.1_bss_enabled", None))
+        elif self._type == "2g_guest_2":
+            self._state = bool_onoff(response.pop("wl0.2_bss_enabled", None))
+        elif self._type == "2g_guest_3":
+            self._state = bool_onoff(response.pop("wl0.3_bss_enabled", None))
+        elif self._type == "5g_wifi":
+            self._state = bool_onoff(response.pop("wl1_bss_enabled", None))
+        elif self._type == "5g_guest_1":
+            self._state = bool_onoff(response.pop("wl1.1_bss_enabled", None))
+        elif self._type == "5g_guest_2":
+            self._state = bool_onoff(response.pop("wl1.2_bss_enabled", None))
+        elif self._type == "5g_guest_3":
+            self._state = bool_onoff(response.pop("wl1.3_bss_enabled", None))
+        elif self._type == "dhcp":
+            self._state = bool_onoff(response.pop("dhcp_enable_x", None))
+        elif self._type == "qos":
+            self._state = bool_onoff(response.pop("qos_enable", None))
+        elif self._type == "model":
+            self._state = response.pop("model", None)
+        elif self._type == "wlan":
+            self._state = response.pop("wan_ipaddr", None)
+        elif self._type == "reboot":
+            self._state = bool_onoff(response.pop("reboot_schedule_enable", None))
+        elif self._type == "firmware":
+            self._state = response.pop("buildno", None)
+        else:
+            self._state = None
+
+        self._attributes = response
+
+
+def bool_onoff(val):
+    """Convert 1|0 to on|off."""
+    if int(val) == 1:
+        return "on"
+    else:
+        return "off"
+
+
+def format_name(name):
+    """Format name."""
+    name = name.replace("_", " ")
+    name = name.title()
+    return name
