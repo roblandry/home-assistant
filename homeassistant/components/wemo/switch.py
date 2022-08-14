072aa996d7a597c3cadd759a425e9cb93ff01f89
roblandry <rob.a.landry@gmail.com>
2022-08-14 13:53:14 -0400

    Rebase

--- homeassistant/components/wemo/switch.py
+++ homeassistant/components/wemo/switch.py
@@ -1,3 +1,4 @@
+# Core version
 """Support for WeMo switches."""
 from __future__ import annotations
 
@@ -5,7 +6,9 @@ import asyncio
 from datetime import datetime, timedelta
 from typing import Any
 
-from pywemo import CoffeeMaker, Insight, Maker, StandbyState, Switch
+# CrockPot +>>
+from pywemo import CrockPot, CoffeeMaker, Insight, Maker, StandbyState, Switch
+# <<
 
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.config_entries import ConfigEntry
@@ -14,7 +17,9 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.dispatcher import async_dispatcher_connect
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN as WEMO_DOMAIN
+# CrockPot +>>
+from .const import DOMAIN as WEMO_DOMAIN, SERVICE_SET_TIMER
+# <<
 from .entity import WemoBinaryStateEntity
 from .wemo_device import DeviceCoordinator
 
@@ -29,9 +34,20 @@ ATTR_ON_TOTAL_TIME = "on_total_time"
 ATTR_POWER_THRESHOLD = "power_threshold_w"
 ATTR_SENSOR_STATE = "sensor_state"
 ATTR_SWITCH_MODE = "switch_mode"
+# CrockPot
+ATTR_CROCKPOT_MODE = "crockpot_mode"
+ATTR_CROCKPOT_TIME = "crockpot_time"
+ATTR_CROCKPOT_COOKEDTIME = "crockpot_cookedtime"
+ATTR_CROCKPOT_TIMESTAMP = "crockpot_timestamp"
+ATTR_TARGET_TIME = "target_time"
+# <<
 
 MAKER_SWITCH_MOMENTARY = "momentary"
 MAKER_SWITCH_TOGGLE = "toggle"
+
+# CrockPot >>
+CROCKPOT_SWITCHES = {"Warm": 50, "Low": 51, "High": 52}
+# <<
 
 
 async def async_setup_entry(
@@ -92,6 +108,21 @@ class WemoSwitch(WemoBinaryStateEntity, SwitchEntity):
         if isinstance(self.wemo, CoffeeMaker):
             attr[ATTR_COFFEMAKER_MODE] = self.wemo.mode
 
+        # Crockpot >>
+        if isinstance(self.wemo, CrockPot):
+
+            if self.crockpot_params:
+                attr[ATTR_CURRENT_STATE_DETAIL] = self.detail_state
+
+            if self.crockpot_params is not None:
+                attr[ATTR_CROCKPOT_MODE] = self.crockpot_params["mode"]
+                attr[ATTR_CROCKPOT_TIME] = self.crockpot_params["time"]
+                attr[ATTR_CROCKPOT_COOKEDTIME] = self.crockpot_params["cookedTime"]
+                t = datetime.fromtimestamp(float(self.crockpot_params["timeStamp"]))
+                attr[ATTR_CROCKPOT_TIMESTAMP] = t.strftime("%Y-%m-%d %H:%M:%S")
+            # _LOGGER.debug(f"Attr: {attr}")
+        # <<
+
         return attr
 
     @staticmethod
@@ -116,6 +147,11 @@ class WemoSwitch(WemoBinaryStateEntity, SwitchEntity):
             if standby_state == StandbyState.STANDBY:
                 return STATE_STANDBY
             return STATE_UNKNOWN
+        # CrockPot >>
+        if isinstance(self.wemo, CrockPot):
+            if self.crockpot_params is not None:
+                return self._mode_string
+        # <<
         assert False  # Unreachable code statement.
 
     @property
@@ -123,14 +159,32 @@ class WemoSwitch(WemoBinaryStateEntity, SwitchEntity):
         """Return the icon of device based on its type."""
         if isinstance(self.wemo, CoffeeMaker):
             return "mdi:coffee"
+        # CrockPot >>
+        if isinstance(self.wemo, CrockPot):
+            return "mdi:pot-steam"
+        # <<
         return None
 
     def turn_on(self, **kwargs: Any) -> None:
         """Turn the switch on."""
         with self._wemo_call_wrapper("turn on"):
-            self.wemo.on()
+            # CrockPot >>
+            if isinstance(self.wemo, CrockPot):
+                self.wemo.set_state(int(CROCKPOT_SWITCHES[self._theSwitch]))
+            else:
+            # <<
+                self.wemo.on()
 
     def turn_off(self, **kwargs: Any) -> None:
         """Turn the switch off."""
         with self._wemo_call_wrapper("turn off"):
             self.wemo.off()
+
+    # CrockPot >>
+    def set_timer(self, **kwargs: Any) -> None:
+        """Set Timer."""
+        with self._wemo_call_wrapper("set timer"):
+            if timer:
+                # self.wemo.set_time(timer)
+                self.wemo.set_state(int(CROCKPOT_SWITCHES[self._theSwitch]), timer)
+    # <<
